23602ecb145d4e01f7c0789fd375d4500cb0cf96
michaelmccoll <mccollmichael@hotmail.com>
2021-03-18 12:05:36 +0000

    MVP completed

---
+++ models/book.py
@@ -0,0 +1,8 @@
+class Book:
+
+    def __init__(self, title, description, author, theme,  id = None):
+        self.title = title
+        self.description = description
+        self.author = author
+        self.theme = theme
+        self.id = id
---
+++ repositories/book_repository.py
@@ -0,0 +1,57 @@
+from db.run_sql import run_sql
+
+from models.book import Book
+from models.author import Author
+import repositories.author_repository as author_repository
+
+
+def save(book):
+    sql = "INSERT INTO books (title, description, author_id, theme) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [book.title, book.description, book.author.id, book.theme]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    book.id = id
+    return book
+
+
+def select_all():
+    books = []
+
+    sql = "SELECT * FROM books"
+    results = run_sql(sql)
+
+    for row in results:
+        author = author_repository.select(row['author_id'])
+        book = Book(row['title'], row['description'], author, row['theme'], row['id'] )
+        books.append(book)
+    return books
+
+
+
+def select(id):
+    book = None
+    sql = "SELECT * FROM books WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        author = author_repository.select(result['author_id'])
+        book = Book(result['description'], result['description'], author, result['theme'], result['id'] )
+    return book
+
+
+def delete_all():
+    sql = "DELETE FROM books"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM books WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(book):
+    sql = "UPDATE books SET (title, description, author_id, theme) = (%s, %s, %s, %s) WHERE id = %s"
+    values = [book.title, book.description, book.author.id, book.theme, book.id]
+    run_sql(sql, values)
